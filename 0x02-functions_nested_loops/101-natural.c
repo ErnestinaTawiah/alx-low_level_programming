1e4dab47e005e3a38588952c4930e88017640b30
ErnestinaTawiah <starkles1181@gmail.com>
2022-03-15 23:04:34 -0700

    natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Lists all the natural numbers below 1024 (excluded)
+ * that are multiples of 3 0r 5.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 int i, sum = 0;
